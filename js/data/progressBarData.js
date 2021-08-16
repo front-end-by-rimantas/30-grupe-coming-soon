ad63375fc8d50749f48e4d82b635258f958158bc
Rimantas <belauzas@yahoo.com>
2021-08-16 12:25:42 +0300

    Corrupted data #7

--- js/data/progressBarData.js
+++ js/data/progressBarData.js
@@ -3,9 +3,21 @@ const progressBarData = [
         label: 'UX Design',
         value: 90,
     },
+    {},
+    {
+        label: 'labas :P',
+    },
+    {
+        value: 'labas :P',
+    },
+    5847152,
     {
         label: 'Web Design',
         value: 86,
+    },
+    {
+        label: [1, 2, 5],
+        value: 'wewrt',
     },
     {
         label: 'Web Development',

16e9c63f50a9c6690dce5eb2cd19edbea9264be9
Rimantas <belauzas@yahoo.com>
2021-08-16 12:17:40 +0300

    Progress bars rendering #7

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import { ProgressBars } from "./components/progress-bar/ProgressBars.js";
+import { progressBarData } from "./data/progressBarData.js";
 
-const pb = new ProgressBars();
-console.log(pb);
+const pb = new ProgressBars('.left-column', progressBarData);
+pb.render();
+
+const pb2 = new ProgressBars('.right-column', progressBarData);
+pb2.render();
